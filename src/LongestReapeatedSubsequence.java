b3db411548b1af9d22b5ae9e206484bd5edc140c
Zhuorui Yang <zhuoruiyang@gmail.com>
2018-02-03 00:50:16 -0500

    LRS is solved

--- src/LongestReapeatedSubsequence.java
+++ src/LongestReapeatedSubsequence.java
@@ -1,18 +1,58 @@
-
+/*
+ * https://www.geeksforgeeks.org/longest-repeated-subsequence/
+ */
 public class LongestReapeatedSubsequence {
 	public static void main(String[] args) {
-		String s = solveLRS("aaabbb");
-		System.out.println(s);
+		String input = "AABEBCDD";
+		System.out.println(solveLRS(input));
+		System.out.println(printLRS(input));
 	}
 	
-	public static String solveLRS(String s) {
-		StringBuilder sb = new StringBuilder();
+	public static int solveLRS(String s) {
+		int[][] dp = new int[2][s.length()+1];
 		for(int i = 0; i < s.length(); i++) {
-			for(int j = 0; j < i; j++) {
-				if(s.charAt(i) == s.charAt(j) && i != j)
-					sb.append(s.charAt(i));
+			for(int j = 0; j < s.length(); j++) {
+				int r = i & 1;
+				if(i != j && s.charAt(i) == s.charAt(j)) {
+					dp[r][j+1] = dp[1-r][j] + 1;
+				}
+				else {
+					dp[r][j+1] = dp[r][j] > dp[1-r][j+1] ? dp[r][j] : dp[1-r][j+1];
+				}
 			}
 		}
-		return sb.toString();
+		return dp[1][s.length()];
+	}
+	
+	public static String printLRS(String s) {
+		int[][] dp = new int[s.length()+1][s.length()+1];
+		for(int i = 0; i < s.length(); i++) {
+			for(int j = 0; j < s.length(); j++) {
+				if(i != j && s.charAt(i) == s.charAt(j)) {
+					dp[i+1][j+1] = dp[i][j] + 1;
+				}
+				else {
+					dp[i+1][j+1] = dp[i+1][j] > dp[i][j+1] ? dp[i+1][j] : dp[i][j+1];
+				}
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		int m = s.length(), n = m;
+		while(m >= 1 && n >= 1) {
+			if(m != n && s.charAt(m-1) == s.charAt(n-1)) {
+				sb.append(s.charAt(m-1));
+				m--;
+				n--;
+			}
+			else {
+				if(dp[m-1][n] > dp[m][n-1]) {
+					m--;
+				}
+				else {
+					n--;
+				}
+			}
+		}
+		return sb.reverse().toString();
 	}
 }
--- src/test.java
+++
@@ -1,8 +0,0 @@
-
-public class test {
-	public static void main(String[] args) {
-		int a = 5;
-		int b = 5&1;
-		System.out.println(b);
-	}
-}
